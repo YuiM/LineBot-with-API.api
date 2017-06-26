d6a039b28ef6a27f5924336a40df0f3cd1c4b08c
mori <mori@localhost.localdomain>
2017-06-26 21:43:38 +0900

    added body-parser

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // -----------------------------------------------------------------------------
-// // モジュールのインポート
+  const LINE_CHANNEL_ACCESS_TOKEN = 'cEZnXk+iZCdy6PledOz2v5NBbRE/oT9gRCV20SWI1gJeBd1+vqDLQbRCtIRmF+IfBajc+wh0dE9GqctEJbLLoo8tw6dJGwAwDVbjvDzHKHxOhimHsQ16Wthtiv5SqfUcTH1i+/pvsukH4vu6JTMNIAdB04t89/1O/w1cDnyilFU='; // 追加
+// // モジュールのインポート 
   var express = require('express');
   var bodyParser = require('body-parser'); // 追加
+  var request = require('request'); // 追加  
   var app = express();
 // // ミドルウェア設定
   app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,8 +25,26 @@
 app.post('/webhook', function(req, res, next){
     res.status(200).end();
     for (var event of req.body.events){
-        if (event.type == 'message'){
-            console.log(event.message);
+        if (event.type == 'message' && event.message.text == 'ハロー'){
+            var headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + LINE_CHANNEL_ACCESS_TOKEN
+            }
+            var body = {
+                replyToken: event.replyToken,
+                messages: [{
+                    type: 'text',
+                    text: 'こんにちはー'
+                }]
+            }
+            var url = 'https://api.line.me/v2/bot/message/reply';
+            request({
+                url: url,
+                method: 'POST',
+                headers: headers,
+                body: body,
+                json: true
+            });
         }
     }
 });
