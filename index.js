0cdc2c1c371349ab27dc9cb1cd196a37f4febe85
mori <mori@localhost.localdomain>
2017-06-26 21:39:03 +0900

    added body-parser

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // -----------------------------------------------------------------------------
 // // モジュールのインポート
   var express = require('express');
+  var bodyParser = require('body-parser'); // 追加
   var app = express();
+// // ミドルウェア設定
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json()); // 追加
+
+
+
 //
 // // -----------------------------------------------------------------------------
 // // Webサーバー設定
@@ -12,26 +19,6 @@
 //
 //     // -----------------------------------------------------------------------------
 //     // ルーター設定
-     app.get('/', function(req, res, next){
-         res.send('Node is running on port ' + port);
-         });
-      app.post('/webhook', function(req, res, next){
-          res.status(200).end();
-          console.log(req.body);
-      });
-// -----------------------------------------------------------------------------
-// // モジュールのインポート
- var express = require('express');
- var bodyParser = require('body-parser'); // 追加
- var app = express();
-//
-//
-// // -----------------------------------------------------------------------------
-// // ミドルウェア設定
- app.use(bodyParser.urlencoded({ extended: false }));
- app.use(bodyParser.json()); // 追加
-
-
 
 app.post('/webhook', function(req, res, next){
     res.status(200).end();
